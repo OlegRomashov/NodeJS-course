d14c6ed685fe2477ab676f70d9b7fe46e2241192
OlegRomashov <dead_line@list.ru>
2020-11-06 16:12:51 +0300

    finished the last task

--- statistics.js
+++ statistics.js
@@ -10,7 +10,6 @@ const rl = readline.createInterface({
 rl.question('Введите название файла?', (name) => {
     name = name + '.txt'
     filePath = path.join(__dirname, 'logs', name)
-    console.log(filePath)
 
     fs.readFile(filePath, (err, content) => {
         if(err) {
@@ -22,6 +21,24 @@ rl.question('Введите название файла?', (name) => {
         console.log('проиграно', data.filter(el => el === 'false').length)
         console.log('соотношение побед к проигрышам', (data.filter(el => el === 'true').length) /
                                                       (data.filter(el => el === 'false').length))
+        function foo(x) {
+            let max = 0
+            let count = 0
+            for (let i = 0; i < data.length; i++) {
+                if (data[i] === x) {
+                    count++
+                } else {
+                    if (count > max) {
+                        max = count
+                    }
+                    count = 0;
+                }
+            }
+            return max
+        }
+
+        console.log('максимальное число побед подряд', foo('true'))
+        console.log('максимальное число проигрышей подряд', foo('false'))
     })
     rl.close()
 })
