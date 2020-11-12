2eafdc92499290d76f50e9f285ce3740447433e9
OlegRomashov <dead_line@list.ru>
2020-11-12 15:26:12 +0300

    completed the second task

--- server.js
+++ server.js
@@ -1,24 +1,44 @@
 const express = require('express');
+const fs = require('fs');
+const yandexTranslate = require('./yandex.js');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.json());
+app.use(bodyParser());
 
-// app.get('/', (req, res) => res.send('Hello, Vasya!'));
+const templating = require('consolidate');
+const handlebars = require('handlebars');
+templating.requires.handlebars = handlebars;
 
-// app.get('/about', (req, res) => res.send('Nothing new about us...'));
+app.engine('hbs', templating.handlebars);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
+app.get('/', (req, res, next) => {
+    res.render('translate', { text: '' });
+    // res.json(cache);
+});
 
-app.get('/', (req, res) => {
-    res.render('index.html')
-})
+app.post('/translate', async(req, res) => {
+    const request = req.body;
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    res.render('index.html')
+    yandexTranslate({
+        texts: [request.text],
+        sourceLanguageCode: request.sourceLanguageCode,
+        targetLanguageCode: request.targetLanguageCode
+    }).then((yres) => {
+        res.render('translate', { text: yres.data.translations[0].text })
+    }).catch((err) => {
+        console.log(err.message, err.response.data);
+        res.status('400').render('translate', { error: err.message })
+    })
 })
 
 const PORT = process.env.PORT || 3000
+
 app.listen(PORT, () => {
     console.log('Server has started on PORT: ', PORT)
 })
--- yandex.js
+++ yandex.js
@@ -1,19 +1,16 @@
 const axios = require('axios');
 
-const IAM_TOKEN = 't1.9f7L7euelZqWi5nKisuWiZOTjZiPmJOZyeX09wMKWgL67y8hT_zd9PdDOFcC-u8vIU_8.PwUie73AJHkrpRoDtYyUiKD2m3lCZ2oi2B75TZaAFsXc9zOOoIPlRz3WsVEGipiZ_C5v__EoHS52_BMF5kzkCA';
+const IAM_TOKEN = 't1.9euelZrPlprIl5iYzc-MiZuekpvGmO3rnpWalouZyorLlomTk42Yj5iTmcnl9PcKcksC-u9fSSqT3fT3SiBJAvrvX0kqkw.do4AC3dKfvAMoQ6NEQXHbIqgz_vWr4ud5oLDflN5sbNk2g38hIrVFZOQxG3t2OB0lCfglKvpC2zreVyjliYgCQ';
 const FOLDER = 'b1gst5qpqhdq36hft3r7'
 
-axios.post('https://translate.api.cloud.yandex.net/translate/v2/translate/', {
-    "folder_id": FOLDER,
-    "texts": ["В этом примере показано, как перевести на русский язык две строки с текстом"],
-    "targetLanguageCode": "en"
-}, {
-    headers: {
-        'Content-Type': 'aaplication/json',
-        'Authorization': 'Bearer ' + IAM_TOKEN
-    }
-}).then((res) => {
-    console.log(res.data);
-}).catch((err) => {
-    console.log(err);
-})
+module.exports = function yandexTranslate(request) {
+    return axios.post('https://translate.api.cloud.yandex.net/translate/v2/translate/', {
+        "folder_id": FOLDER,
+        ...request
+    }, {
+        headers: {
+            'Content-Type': 'aaplication/json',
+            'Authorization': 'Bearer ' + IAM_TOKEN
+        }
+    })
+}
